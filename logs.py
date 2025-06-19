e674036beb6f6f4232e2687bcaf9b3ca3239a4ac
Armesa <jannikhuppertz22@gmail.com>
2025-06-19 11:48:41 +0200

    log fix

--- logs.py
+++ logs.py
@@ -1,8 +1,11 @@
 ### logs.py
 import logging
 from datetime import datetime
+import os
+# Stelle sicher, dass der Log-Ordner existiert, bevor das Logging initialisiert wird
+os.makedirs("logs", exist_ok=True)
 
-logging.basicConfig(filename='logs/bot.log', level=logging.DEBUG)
+logging.basicConfig(filename="logs/bot.log", level=logging.DEBUG)
 
 def debug(message):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
